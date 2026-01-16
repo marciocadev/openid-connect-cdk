46b17064803b5e20838d8d9314d591fa58e1e795
Marcio Cruz de Almeida <marciocadev@gmail.com>
2026-01-16 17:54:29 -0300

    first commit

--- lib/openid-connect-cdk-stack.ts
+++ lib/openid-connect-cdk-stack.ts
@@ -1,16 +1,48 @@
-import * as cdk from 'aws-cdk-lib/core';
+import { ManagedPolicy, OidcProviderNative, OpenIdConnectPrincipal, Role } from 'aws-cdk-lib/aws-iam';
+import { CfnOutput, Duration, Stack, StackProps } from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class OpenidConnectCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+const REPO_LIST: { owner: string, repo?: string, filter?: string }[] = [
+  {
+    owner: "marciocadev",
+    repo: "openid-connect-cdk",
+  },
+];
+
+export class OpenidConnectCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const gitHubDomain = "token.actions.githubusercontent.com";
+    const gitHubOidcProvider = new OidcProviderNative(this, "GitHubOidcProvider", {
+      url: `https://${gitHubDomain}`,
+      clientIds: ["sts.amazonaws.com"],
+      oidcProviderName: "GithubOidcProvider",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'OpenidConnectCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const repoSub = REPO_LIST.map(
+      (r) => `repo:${r.owner}/${r.repo}:${r.filter ?? '*'}`
+    );
+
+    const oidcRole = new Role(this, "GitHubOidcRole", {
+      roleName: "GitHubOidcRole",
+      assumedBy: new OpenIdConnectPrincipal(gitHubOidcProvider, {
+        StringEquals: {
+          [`${gitHubDomain}:aud`]: ['sts.amazonaws.com']
+        },
+        StringLike: {
+          [`${gitHubDomain}:sub`]: repoSub,
+        },
+      }),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')
+      ],
+      maxSessionDuration: Duration.hours(1),
+    });
+
+    new CfnOutput(this, 'GithubOidcRoleOutput', {
+      value: oidcRole.roleArn,
+      exportName: 'GithubOidcRoleArn',
+    });
   }
 }
