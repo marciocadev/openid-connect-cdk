e565730ae9fcb9086939dbf4815745c46de17699
Marcio Cruz de Almeida <marciocadev@gmail.com>
2026-01-16 17:36:00 -0300

    Initial commit

---
+++ test/openid-connect-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OpenidConnectCdk from '../lib/openid-connect-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/openid-connect-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OpenidConnectCdk.OpenidConnectCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
